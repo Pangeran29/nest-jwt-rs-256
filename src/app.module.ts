e08a528b8516823b86d2705736343ca7b2a356d4
Pangeran29 <pangeranjonathan1@gmail.com>
2023-01-13 12:05:06 +0700

    feat: Register algorithm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as fs from 'fs';
+
+const privateKey  = fs.readFileSync('private.pem');
+const publicKey  = fs.readFileSync('public.pem');
 
 @Module({
-  imports: [],
+  imports: [
+    // register jwt module
+    JwtModule.registerAsync({
+      useFactory: async () => {
+        const options: JwtModuleOptions = {
+          privateKey: privateKey,
+          publicKey: publicKey,
+          signOptions: {
+            expiresIn: '24h',
+            algorithm: 'RS256',
+          },
+        };
+        return options;
+      }
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt/dist';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly jwtService: JwtService
+  ) {}
+
+  async testJwt(): Promise<{ token: string }> {
+    const token = await this.jwtService.signAsync({
+      data: { email: 'java', role: '1' }
+    });
+
+    return { token };
   }
 }
